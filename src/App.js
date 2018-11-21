3dbbdd7f686ade55e0d6aae10744b61f05b9d12e
Doron Sharon <doron@dorsha.com>
2018-11-21 23:14:54 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="App-logo">ZG</div>
+          <h1>Dr. Zohar Goichberg</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
